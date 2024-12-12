6966f0b553e0153c4630f351add897e0b459ce5b
beshprakash11 <beshprakashktm@gmail.com>
2024-12-12 10:58:03 +0100

    mantra repo updated

---
+++ meditation/Infrastructure/Repository/Implementation/MantraRepository.cs
@@ -0,0 +1,42 @@
+﻿using meditation.Core.Models.Domain;
+using meditation.Infrastructure.Repository.Interface;
+
+namespace meditation.Infrastructure.Repository.Implementation
+{
+    public class MantraRepository : IMantraRepository
+    {
+        public MantraRepository()
+        {
+            
+        }
+        public Task<MantraModel> CreateMantrasAsync(MantraModel mantras)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<IEnumerable<MantraModel>> GetAllMantrasAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MantraModel> GetMantrasByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MantraModel> GetMantrasByNameAsync(string mantraname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MantraModel> UpdateMantrasAsync(MantraModel mantras)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<MantraModel> DeleteMantrasAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ meditation/Infrastructure/Repository/Interface/IMantraRepository.cs
@@ -0,0 +1,14 @@
+﻿using meditation.Core.Models.Domain;
+
+namespace meditation.Infrastructure.Repository.Interface
+{
+    public interface IMantraRepository
+    {
+        Task<MantraModel> CreateMantrasAsync(MantraModel mantras);
+        Task<IEnumerable<MantraModel>> GetAllMantrasAsync();
+        Task<MantraModel> GetMantrasByIdAsync(Guid id);
+        Task<MantraModel> GetMantrasByNameAsync(string mantraname);
+        Task<MantraModel> UpdateMantrasAsync(MantraModel mantras);
+        Task<MantraModel> DeleteMantrasAsync(Guid id);
+    }
+}
