4f83f992181e50da143605c0d503e00d8591d61e
beshprakash11 <beshprakashktm@gmail.com>
2024-12-12 10:47:06 +0100

    StoreContext created

---
+++ meditation/Infrastructure/DataStoreContext/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace meditation.Infrastructure.DataStoreContext
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
