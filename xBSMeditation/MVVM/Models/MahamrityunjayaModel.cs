4c5008e8c296bf22f938d9b761d043f2020e28af
beshprakash11 <beshprakashktm@gmail.com>
2024-12-12 09:34:57 +0100

    MahamrityunjayaModel updated

--- xBSMeditation/MVVM/Models/MahamrityunjayaModel.cs
+++ xBSMeditation/MVVM/Models/MahamrityunjayaModel.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace xBSMeditation.MVVM.Models
 {
-    public class MahamrityunjayaModel
+    public class MahamrityunjayaModel:BaseModel
     {
+        public int NumberOfMantraCount { get; set; } //10 mala /day
+        public int NumberOfBedInMala { get; set; } //108
+        public int TotalNumberOfJapa { get; set; } // 10*108 = 1080
+        public DateTime CreatedAt { get; set; }
     }
 }
