8b506da5046a5fc09f2f1866c5de4d460aea308e
beshprakash11 <beshprakashktm@gmail.com>
2024-12-12 09:36:42 +0100

    property updated

--- xBSMeditation/MVVM/Models/ShivaMantraModel.cs
+++ xBSMeditation/MVVM/Models/ShivaMantraModel.cs
@@ -8,5 +8,9 @@ namespace xBSMeditation.MVVM.Models
 {
     public class ShivaMantraModel : BaseModel
     {
+        public int NumberOfMantraCount { get; set; } //10 mala /day
+        public int NumberOfBedInMala { get; set; } //108
+        public int TotalNumberOfJapa { get; set; } // 10*108 = 1080
+        public DateTime CreatedAt { get; set; }
     }
 }
