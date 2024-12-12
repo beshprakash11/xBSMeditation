30dada4851652dd3dfebc5a470c8ed85d0d5806d
beshprakash11 <beshprakashktm@gmail.com>
2024-12-12 10:59:22 +0100

    StoreContext updated

--- meditation/Infrastructure/DataStoreContext/StoreContext.cs
+++ meditation/Infrastructure/DataStoreContext/StoreContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using meditation.Core.Models.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace meditation.Infrastructure.DataStoreContext
 {
@@ -6,9 +7,22 @@ namespace meditation.Infrastructure.DataStoreContext
     {
         public StoreContext(DbContextOptions options) : base(options) { }
 
+        public DbSet<MantraModel> Mantras { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            //fixing decimal problem in sqlite
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
