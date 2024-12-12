be621566497898669811d096d628f03c21166449
beshprakash11 <beshprakashktm@gmail.com>
2024-12-12 09:40:57 +0100

    krishna mantra model created

---
+++ xBSMeditation/MVVM/Models/KrishnaMantraModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xBSMeditation.MVVM.Models
+{
+    public class KrishnaMantraModel:BaseModel
+    {
+        public int NumberOfMantraCount { get; set; } //10 mala /day
+        public int NumberOfBedInMala { get; set; } //108
+        public int TotalNumberOfJapa { get; set; } // 10*108 = 1080
+        public DateTime CreatedAt { get; set; }
+    }
+}
