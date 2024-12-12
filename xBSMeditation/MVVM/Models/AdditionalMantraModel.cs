e9c02b002a8b7b111e3840b665fef26fe2e5d8ab
beshprakash11 <beshprakashktm@gmail.com>
2024-12-12 09:42:24 +0100

    AdditionalMantraModel updated

---
+++ xBSMeditation/MVVM/Models/AdditionalMantraModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xBSMeditation.MVVM.Models
+{
+    public class AdditionalMantraModel
+    {
+        public string Name { get; set; }
+        public int NumberOfMantraCount { get; set; } //10 mala /day
+        public int NumberOfBedInMala { get; set; } //108
+        public int TotalNumberOfJapa { get; set; } // 10*108 = 1080
+        public DateTime CreatedAt { get; set; }
+    }
+}
