c4b1329352528423a32e02359b08706ee17d58b0
beshprakash11 <beshprakashktm@gmail.com>
2024-12-12 09:58:26 +0100

    project updated

--- xBSMeditation/MVVM/Models/MantraModel.cs
+++ xBSMeditation/MVVM/Models/MantraModel.cs
@@ -8,5 +8,13 @@ namespace xBSMeditation.MVVM.Models
 {
     public class MantraModel:BaseModel
     {
+        public string MantraName { get; set; }
+        public string  MantraImage { get; set; }
+        public string MantraAudio { get; set; }
+        public string MantraDescription { get; set; }
+        public string LordImage { get; set; }
+        public string LordThreed { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
