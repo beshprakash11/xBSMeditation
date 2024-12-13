f6006df690a2a28b7c4a926bd78cc216203e7a3f
beshprakash11 <beshprakashktm@gmail.com>
2024-12-13 11:15:12 +0100

    MahamrityunjayaModelCreateDto updated

--- meditation/Core/Models/Dto/CreateDto/MahamrityunjayaModelCreateDto.cs
+++ meditation/Core/Models/Dto/CreateDto/MahamrityunjayaModelCreateDto.cs
@@ -2,5 +2,9 @@
 {
     public class MahamrityunjayaModelCreateDto:BaseModel
     {
+        public int NumberOfMantraCount { get; set; } //10 mala /day
+        public int NumberOfBedInMala { get; set; } //108
+        public int TotalNumberOfJapa { get; set; } // 10*108 = 1080
+        public DateTime CreatedAt { get; set; }
     }
 }
