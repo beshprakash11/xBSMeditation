daec813e2af65064ff36c56eddb6f4092330e21e
beshprakash11 <beshprakashktm@gmail.com>
2024-12-13 11:16:33 +0100

    public int NumberOfMantraCount { get; set; } //10 mala /day

---
+++ meditation/Core/Models/Domain/ShivaMantraModel.cs
@@ -0,0 +1,10 @@
+﻿namespace meditation.Core.Models.Domain
+{
+    public class ShivaMantraModel:BaseModel
+    {
+        public int NumberOfMantraCount { get; set; } //10 mala /day
+        public int NumberOfBedInMala { get; set; } //108
+        public int TotalNumberOfJapa { get; set; } // 10*108 = 1080
+        public DateTime CreatedAt { get; set; }
+    }
+}
