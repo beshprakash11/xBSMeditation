65ac14e673ce1ac050da542a145b67884331fb20
beshprakash11 <beshprakashktm@gmail.com>
2024-12-13 08:43:09 +0100

    MantraModel updated

--- meditation/Core/Models/Domain/MantraModel.cs
+++ meditation/Core/Models/Domain/MantraModel.cs
@@ -1,14 +1,28 @@
-﻿namespace meditation.Core.Models.Domain
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace meditation.Core.Models.Domain
 {
     public class MantraModel:BaseModel
     {
         public string MantraName { get; set; }
-        public string MantraImage { get; set; }
-        public string MantraAudio { get; set; }
+        public string MantraImagePath { get; set; }
+        public string MantraAudioPath { get; set; }
         public string MantraDescription { get; set; }
-        public string LordImage { get; set; }
-        public string LordThreed { get; set; }
+        public string LordImagePath { get; set; }
+        public string LordThreedPath { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+
+        [NotMapped] // Prevents this property from being mapped to the database
+        public IFormFile MantraImage { get; set; }
+
+        [NotMapped]
+        public IFormFile MantraAudio { get; set; }
+
+        [NotMapped]
+        public IFormFile LordImage { get; set; }
+
+        [NotMapped]
+        public IFormFile LordThreed { get; set; }
     }
 }
