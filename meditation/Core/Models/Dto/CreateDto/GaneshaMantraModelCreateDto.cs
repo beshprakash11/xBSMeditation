e0173fac8d5260c105dd08f399d3ec03c0a1a990
beshprakash11 <beshprakashktm@gmail.com>
2024-12-13 11:08:35 +0100

    updated

--- meditation/Core/Models/Dto/CreateDto/GaneshaMantraModelCreateDto.cs
+++ meditation/Core/Models/Dto/CreateDto/GaneshaMantraModelCreateDto.cs
@@ -1,6 +1,10 @@
 ﻿namespace meditation.Core.Models.Dto.CreateDto
 {
-    public class GaneshaMantraModelCreateDto
+    public class GaneshaMantraModelCreateDto:BaseModel
     {
+        public int NumberOfMantraCount { get; set; } //10 mala /day
+        public int NumberOfBedInMala { get; set; } //108
+        public int TotalNumberOfJapa { get; set; } // 10*108 = 1080
+        public DateTime CreatedAt { get; set; }
     }
 }
