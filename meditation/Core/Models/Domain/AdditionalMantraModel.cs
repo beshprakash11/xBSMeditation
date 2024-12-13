1b36b2c4125930106548094538de758e17a37b23
beshprakash11 <beshprakashktm@gmail.com>
2024-12-13 11:04:53 +0100

    AdditionalMantraModel updated

--- meditation/Core/Models/Domain/AdditionalMantraModel.cs
+++ meditation/Core/Models/Domain/AdditionalMantraModel.cs
@@ -1,6 +1,11 @@
 ﻿namespace meditation.Core.Models.Domain
 {
-    public class AdditionalMantraModel
+    public class AdditionalMantraModel:BaseModel
     {
+        public string MantraName { get; set; }
+        public int NumberOfMantraCount { get; set; } //10 mala /day
+        public int NumberOfBedInMala { get; set; } //108
+        public int TotalNumberOfJapa { get; set; } // 10*108 = 1080
+        public DateTime CreatedAt { get; set; }
     }
 }
