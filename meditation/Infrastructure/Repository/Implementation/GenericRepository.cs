1193d1a79dda7522cc14c67b9871259f15796ae9
beshprakash11 <beshprakashktm@gmail.com>
2024-12-14 15:19:56 +0100

    generic repo updated

---
+++ meditation/Infrastructure/Repository/Implementation/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using meditation.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using meditation.Infrastructure.Repository.Interface;
+
+namespace meditation.Infrastructure.Repository.Implementation
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseModel
+    {
+        public Task<T> CreateAsync(T TEntity)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> UpdateByIdAsync(T TEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> DeleteByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ meditation/Infrastructure/Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using meditation.Core.Models;
+
+namespace meditation.Infrastructure.Repository.Interface
+{
+    public interface IGenericRepository<T> where T : BaseModel
+    {
+        Task<T> CreateAsync(T TEntity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(Guid id);
+        Task<T?> UpdateByIdAsync(T TEntity);
+        Task<T?> DeleteByIdAsync(Guid id);
+    }
+}
